8a0e867798ca41903ef1a8b6ab5644895e12520c
DavidForDev <87243091+DavidForDev@users.noreply.github.com>
2022-08-31 15:53:48 +0400

    Add files via upload

---
+++ src/components/productCart/productCart.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from 'react';
+import './productCart.style.scss';
+import {Link} from 'react-router-dom'
+
+// Import SVG icons
+
+import Close from '../../assets/icons/close';
+
+// Import Component
+import ProductCartBox from './productCartBox/productCartBox';
+
+const ProductCart = ({cartItem, removeFromCart, totalPrice}) => {
+    const [disabled, setDisabled] = useState(true);
+    
+    useEffect(() => {
+        if(cartItem.length >= 1) {
+            setDisabled(false)
+        } else {
+            setDisabled(true)
+        }
+    }, [cartItem])
+
+    function closer() {
+        const prod = document.querySelector('.black__opacity-bg');
+        prod.style.display = 'none'
+    }
+    
+
+    return ( 
+        <div className='black__opacity-bg'>
+            <div className='product__cart'>
+                <div className='product_cart-section flex align-items-center justify-between'>
+                    <div className='flex align-items-center' style={{gap: '15px'}}>
+                        <h4 className='cart-title'>Your Cart</h4>
+                        <p className='low font-underline'>{cartItem.length ? cartItem.length : '0'} items</p>
+                    </div>
+                    <div onClick={closer}>
+                        <Close width='15' height='15'/>
+                    </div>
+                </div>
+                <div className='product_cart-section'>
+                    <div className='product_in_cart'>
+                        {cartItem.length >= 1 ? 
+                        cartItem.map((e) => {
+                            return (
+                                <ProductCartBox cart={e} removeFromCart={removeFromCart} key={e._id}/>
+                            )
+                        })   
+                        : <div className='text-center'>
+                            <h3>Cart is empty</h3>
+                            <p>Please choose product</p>    
+                        </div>}
+                    </div>
+                </div>
+                <div className='product_cart-section'>
+                    <div className='product_cart-checkout'>
+                        <div className='flex align-items-center justify-between'>
+                            <span className='font-bold'>Subtotal</span>
+                            <p>${totalPrice}</p>
+                        </div>
+                        <div className='flex align-items-center justify-between'>
+                            <span className='font-bold flex'>Tax <p className='font-bold-400'>(calculated in checkout)</p></span>
+                            <p>--</p>
+                        </div>
+                        <div className='flex align-items-center justify-between'>
+                            <span className='font-bold flex'>Shipping <p className='font-bold-400'>(free standard over $40)</p></span>
+                            <p>Free</p>
+                        </div>
+                    </div>
+                    <div className='finally-checkout flex align-items-center justify-between'>
+                        <h5>Estimated Total</h5>
+                        <span>${totalPrice}</span>
+                    </div>
+                    <div className='text-center'>
+                        <a href='/checkout'>
+                            <button className='primary_btn' disabled={disabled}>Checkout</button>
+                        </a>
+                        <a href='###'>View Cart</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default ProductCart;
